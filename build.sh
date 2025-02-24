9162282b7931333d2dd9b2c4bec66a2007b70585
achiekh <chiekh@revly.ae>
2025-02-24 15:59:44 +0400

    try sth with build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 nix-shell -p openssl pkg-config --run "cargo build --release"
-
+export OPENSSL_DIR=$(nix eval nixpkgs.openssl --raw)
+export OPENSSL_INCLUDE_DIR=$OPENSSL_DIR/include
+export OPENSSL_LIB_DIR=$OPENSSL_DIR/lib
+export PKG_CONFIG_PATH=$OPENSSL_DIR/lib/pkgconfig
+export PKG_CONFIG_ALLOW_CROSS=1
+export OPENSSL_NO_VENDOR=1
 
